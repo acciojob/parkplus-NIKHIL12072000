98146ea0cb88fe9b0a679745163017ccb9b20ac2
Nikhil <dachepallinikhil@gmail.com>
2023-03-30 23:57:47 +0530

    Tasks Completed

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -38,7 +38,6 @@ public class ParkingLotServiceImpl implements ParkingLotService {
 
         parkingLot.getSpotList().add(spot);
         parkingLotRepository1.save(parkingLot);
-        spotRepository1.save(spot);
         return spot;
     }
 
